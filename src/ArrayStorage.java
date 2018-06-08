c81d649b4139387ec14987c02158aa4862182478
student <karimovrafael-1996@mail.ru>
2018-06-08 10:46:33 +0300

    v.1.0

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,16 +1,29 @@
+import java.util.Arrays;
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int size = 0;
 
     void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
     }
 
-    void save(Resume r) {
+    void save(Resume resume) {
+        if (size < storage.length) {
+            storage[size] = resume;
+            size++;
+        }
     }
 
     Resume get(String uuid) {
+        for (int i=0;i<size;i++){
+            if(storage[i].uuid==uuid){
+                return storage[i];
+            }
+        }
         return null;
     }
 
